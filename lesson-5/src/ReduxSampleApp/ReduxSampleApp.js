ab9f093f722e86105eda1d9e127a6754e0a5b5bf
Sergei Zelinsky <sergei.zelinsky.dev@gmail.com>
2019-11-25 10:33:39 +0200

    update preset for lesson-5

--- lesson-5/src/ReduxSampleApp/ReduxSampleApp.js
+++ lesson-5/src/ReduxSampleApp/ReduxSampleApp.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import * as Actions from './actions';
 import './index.css';
 
 const ReduxSampleApp = ({value, increment, decrement, incrementBy}) => {
   return (
     <div>
       <h2>Counter value: {value}</h2>
-      <button className="action-button" onClick={increment}>
-        Increment
-      </button>
-      <button className="action-button" onClick={decrement}>
-        Decrement
-      </button>
+      <button className="action-button">Increment</button>
+      <button className="action-button">Decrement</button>
 
-      <button className="action-button" onClick={() => incrementBy(2)}>
-        Increment By 2
-      </button>
+      <button className="action-button">Increment By 2</button>
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  value: state,
-});
-
-const ConnectedReduxSampleApp = connect(mapStateToProps, {
-  increment: Actions.increment,
-  decrement: Actions.decrement,
-  incrementBy: Actions.incrementBy,
-})(ReduxSampleApp);
-
-export default ConnectedReduxSampleApp;
+export default ReduxSampleApp;
--- lesson-5/src/ReduxSampleApp/index.js
+++ lesson-5/src/ReduxSampleApp/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import reducer from './reducer';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
 import ReduxSampleApp from './ReduxSampleApp';
 
-const store = createStore(reducer);
-
-const App = () => (
-  <Provider store={store}>
-    <ReduxSampleApp />
-  </Provider>
-);
+const App = () => <ReduxSampleApp />;
 
 export default App;
